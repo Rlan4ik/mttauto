f1c98b24ac0a878f2f9fddc1f9522d1c7cf36f76
Rlan4ik <theruslanglotov@gmail.com>
2025-11-28 17:51:07 +0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,1 +1,10 @@
-print("mtt auto backend is working...")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "mtt auto backend работает 🚗"
+
+if name == "__main__":
+    app.run(debug=True)
