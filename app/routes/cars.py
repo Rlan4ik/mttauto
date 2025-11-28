6da9053bff49b42b60930533aaeb025c8a52d8e4
Rlan4ik <theruslanglotov@gmail.com>
2025-11-28 17:53:07 +0300

    Update cars.py

--- app/routes/cars.py
+++ app/routes/cars.py
@@ -1,1 +1,14 @@
+from flask import Blueprint, jsonify
+from app.models.car import Car
 
+cars_blueprint = Blueprint("cars", __name__)
+
+@cars_blueprint.route("/cars")
+def get_cars():
+    example_cars = [
+        Car("Toyota", "Camry", 2018, 1550000),
+        Car("BMW", "X5", 2016, 2300000),
+        Car("Kia", "Rio", 2020, 950000)
+    ]
+
+    return jsonify([vars(car) for car in example_cars])
