c5941682603160c58f9c996cee86a1fa6e807a11
Rlan4ik <theruslanglotov@gmail.com>
2025-11-28 17:52:11 +0300

    Update car.py

--- app/modes/car.py
+++ app/modes/car.py
@@ -1,1 +1,9 @@
+class Car:
+    def __init__(self, brand, model, year, price):
+        self.brand = brand
+        self.model = model
+        self.year = year
+        self.price = price
 
+    def __repr__(self):
+        return f"{self.brand} {self.model} ({self.year}) — {self.price} руб."
